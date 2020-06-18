bb14faa98b659f9861da5e7dac99dc377000c49a
Jesús Gándara Martínez <jesusalxen@gmail.com>
2020-06-18 20:17:39 +0200

    feat: Update production build

---
+++ docs/precache-manifest.f95ae8e475bc2376013c59ba7ee6f360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbd32a56c7fa54fe1ffa722b5fd9102",
+    "url": "/deepmergefnexample/index.html"
+  },
+  {
+    "revision": "7665cb0f6410bad0be80",
+    "url": "/deepmergefnexample/static/css/main.cb61601c.chunk.css"
+  },
+  {
+    "revision": "89cb2691d67aea872258",
+    "url": "/deepmergefnexample/static/js/2.64b954ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deepmergefnexample/static/js/2.64b954ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7665cb0f6410bad0be80",
+    "url": "/deepmergefnexample/static/js/main.690fa54d.chunk.js"
+  },
+  {
+    "revision": "969cfeee3d34c9e98f02",
+    "url": "/deepmergefnexample/static/js/runtime-main.a48a0b35.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39e34a4120c2e158307cb601a92c358d.js"
+  "/deepmergefnexample/precache-manifest.f95ae8e475bc2376013c59ba7ee6f360.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deepmergefnexample/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
